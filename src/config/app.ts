77abe0492ad010cce9ffb2d8418fbf4ffaa1558a
unknown <crafi245@gmail.com>
2023-07-13 22:57:34 +0600

    configuration done

---
+++ src/config/app.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unreachable */
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+// import routes from '../app/routes';
+import httpStatus from 'http-status';
+import globalErrorHandler from '../app/middleware/globalErrorHandler';
+
+const app: Application = express();
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// application route
+// console.log(process.env)
+// app.use('/api/v1', routes);
+
+// app.get('/', (req: Request, res: Response,next) => {
+// //  throw new ApiError(400,'hello world')
+//  next('hello')
+// })
+// global error handler
+// eslint-disable-next-line no-undef
+app.use(globalErrorHandler);
+
+// handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    sucess: 'False',
+    message: 'Not Found !',
+    erroMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api not found'
+      }
+    ]
+  });
+});
+export default app;
---
+++ src/config/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './index';
+import { logger, errorLogger } from '../shared/logger';
+async function mainserver() {
+  try {
+    await mongoose.connect(config.databse_url as string);
+    logger.info('Database Connection Successful');
+
+    app.listen(config.port, () => {
+      logger.info(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    errorLogger.error('Failled to connect', error);
+  }
+}
+mainserver();
